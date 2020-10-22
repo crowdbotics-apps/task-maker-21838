7f0ec5269518046eab1b59e50ad95142284f4014
Crowdbotics <team@crowdbotics.com>
2020-10-22 18:28:05 +0000

    Update models

--- backend/task_maker_21838/wsgi.py
+++ backend/task_maker_21838/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_maker_21838.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_maker_21838.settings")
 
 application = get_wsgi_application()
